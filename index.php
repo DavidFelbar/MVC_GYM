b9b02b6e70009795359fe1559267c592edbe5e05
david.felbar <david.felbar@DESKTOP-B8B03J9>
2020-06-12 19:04:00 +0200

    test commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-    require_once './routes/Routes.php';
-    function autoloader($class_name){
+   require_once './klase/Route.php';
+   require_once './routes/Routes.php';
+    function moj_autoloader($class_name){
         require_once 'klase/'.$class_name.'.php';
     }
-    spl_autoload_register('autoloader');
+    spl_autoload_register('moj_autoloader');
--- routes/Routes.php
+++ routes/Routes.php
@@ -1,7 +1,16 @@
 <?php
-    Route::set('pocetna',function(){
-        echo 'Pocetna';
-    });
+
+Route::set('Pocetna',function(){
+    echo 'Pocetna';
+});
+
+Route::set('Programi',function(){
+    echo 'Programi';
+});
+
+Route::set('Clanarina',function(){
+    echo 'Clanarina';
+});
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
