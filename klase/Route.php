7d81d6425a7a842f5e4832772a6dd89f85d423d8
david.felbar <david.felbar@DESKTOP-B8B03J9>
2020-06-12 18:41:13 +0200

    test commit

---
+++ klase/Route.php
@@ -0,0 +1,19 @@
+<?php
+
+class Route {
+
+    public static $valid_routes = array();
+
+    public static function set($route, $function) {
+        self::$valid_routes[] = $route;
+        print_r(self::$valid_routes);
+    }
+
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
