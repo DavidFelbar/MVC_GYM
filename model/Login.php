40a4b190f740886cfcd28efc7decc2e19e85546a
david.felbar <david.felbar@DESKTOP-B8B03J9>
2020-06-16 14:12:30 +0200

    Merge origin/master
    Conflicts:
    	Admin/ad_view.php
    	controller/ClanoviController.php

--- model/Login.php
+++ model/Login.php
@@ -1,32 +1,42 @@
 <?php
+
 require_once('../klase/DB.php');
 $db = DB::connect();
 session_start();
 
 $username = $_POST['username'];
-$pass = password_verif($_POST['pass'],PASSWORD_DEFAULT);
+$pass = $_POST['pass'];
 
 
-$upit = "SELECT * FROM clanovi WHERE usernameClana = '$username' AND 
-		 passwordClana = '$pass' LIMIT 1";
+$upit = "SELECT * FROM clanovi WHERE usernameClana = '$username'  LIMIT 1";
 $r = $db->query($upit);
 
 
 if ($r && $r->num_rows == 1) {
-    
+
     $row = $r->fetch_assoc();
-    $_SESSION['login'] = $username;
-    $_SESSION['idProg'] = $row['idProg'];
-    $_SESSION['idClana'] = $row['idClana'];
-    $idK=$row['idKor'];
-    $upit2="SELECT * from tip_korisnika where idKor=$idK";
-    $i=$db->query($upit2);
-    $red=$i->fetch_assoc();
-    $cookieName="Status";
-    setcookie($cookieName, $red['tipKor'],time() + (86400 * 30),'/');
-    
-    header("Location: ../Admin/Clanovi.php");
+    if (password_verify($pass, $row['passwordClana'])) {
+        $_SESSION['login'] = $username;
+        $_SESSION['idProg'] = $row['idProg'];
+        $_SESSION['idClana'] = $row['idClana'];
+        $idK = $row['idKor'];
+        $upit2 = "SELECT * from tip_korisnika where idKor=$idK";
+        $i = $db->query($upit2);
+        $red = $i->fetch_assoc();
+        $cookieName = "Status";
+        setcookie($cookieName, $red['tipKor'], time() + (86400 * 30), '/');
+
+        header("Location: ../Admin/Clanovi.php");
+    } else {
+        
+        
+        header("Location: ../index.php");
+        
+    }
 } else {
+    
+    
     header("Location: ../index.php");
+    
 }
 ?>
