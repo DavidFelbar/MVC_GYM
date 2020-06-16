df70fba6661ce475f8d67aa53b9d7e763277c4ab
david.felbar <david.felbar@DESKTOP-B8B03J9>
2020-06-16 19:57:46 +0200

    Merge origin/master
    Conflicts:
    	Admin/ad_view.php
    	controller/ClanoviController.php

--- view/loginClanovi.php
+++ view/loginClanovi.php
@@ -3,6 +3,34 @@
 <title>Untitled Document</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <style type="text/css">
+                select{
+                padding-right: 10px;
+                margin-left: 20px;
+            }
+            input[type=submit]{
+                margin-left: 20px;
+                width: 140px;
+                height: 30px;
+                color:white;
+                background-color: #333;
+            }
+            input[type=text]:focus{
+               background-color:  #bee6eb;
+            }
+            label{
+                 font-size: 13px;
+            }
+            input[type=radio]{
+                
+               margin-left:30px;
+            }
+            input[type=submit]:hover{
+                
+                background-color: #009cad;
+            }
+            input[type=password]:focus{
+                background-color:  #bee6eb;
+            }
             body {
                 margin:0;
                
