cff5dd55324bf14a44ad503acd1d711f303d1309
Korisnik <Korisnik@DESKTOP-SALHMJE>
2020-06-16 11:03:09 +0200

    Merge origin/final into final
    Conflicts:
    	Admin/Clanovi.php
    	Admin/cl_view.php
    	controller/ProgramiController.php
    	controller/ZaposlenikController.php

--- Admin/Clanovi.php
+++ Admin/Clanovi.php
@@ -47,13 +47,7 @@ switch($a)
        $program = $pp->dohvatiProgram($_SESSION['idProg']);
         $programi = $pr->dohvatiSvePrograme();
         $zaposlenik = $zp->dohvatiZaposlenike();
-       
+
         $template = 'cl_view';
 }
 include './indexClanovi.php';
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
